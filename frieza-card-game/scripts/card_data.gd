981f6b804e80771fdd16b22fee8c4a513064edc3
renaud <75610434+RenaudDeroubaix@users.noreply.github.com>
2024-09-28 13:52:41 +0200

    Menu  + plateau de jeu

---
+++ frieza-card-game/scripts/card_data.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var cards_data = {
+	"Card1": {"name": "Napoleon", "texture": preload("res://asset/napoleon.png")},
+	"Card2": {"name": "Cleopatre", "texture": preload("res://asset/cleopatre.png")},
+}
---
+++ frieza-card-game/scripts/player_data.gd
@@ -0,0 +1,4 @@
+# GameData.gd (le script singleton)
+extends Node
+
+var players = []  # Liste des joueurs avec leurs noms et couleurs
