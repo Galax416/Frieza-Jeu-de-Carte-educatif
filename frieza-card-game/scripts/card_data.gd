7ce37aa469ce163cce30c9a52585932ed1beb217
Renaud Deroubaix <renaud.deroubaix@etu.umontpellier.fr>
2024-10-03 14:51:54 +0200

    gameloop

--- frieza-card-game/scripts/card_data.gd
+++ frieza-card-game/scripts/card_data.gd
@@ -1,6 +1,6 @@
 extends Node
 
 var cards_data = {
-	"Card1": {"name": "Napoleon", "texture": preload("res://asset/napoleon.png")},
-	"Card2": {"name": "Cleopatre", "texture": preload("res://asset/cleopatre.png")},
+	"0": {"name": "Napoleon", "texture": preload("res://asset/napoleon.png"),"date": 1769},
+	"1": {"name": "Cleopatre", "texture": preload("res://asset/cleopatre.png"),"date": -69},
 }
